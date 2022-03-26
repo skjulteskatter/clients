516073947f061df64f77ae83ff0d622239be8dde
Fredrik V <fredrik@vedvik.tech>
2022-03-26 14:55:53 +0100

    1.4.3

--- hiddentreasures-js/src/models/baseObject.ts
+++ hiddentreasures-js/src/models/baseObject.ts
@@ -1,18 +1,18 @@
-export default abstract class BaseObject {
-    private static assignProperties(target: any, source: any) {
-        for (const [key, value] of Object.entries(source)) {
-            if (typeof(target[key]) === "undefined") {
-                target[key] = value;
-            } else if (typeof(target[key]) === "object" && typeof(value) === "object") {
-                if (Array.isArray(target[key])) {
-                    continue;
-                }
-                this.assignProperties(target[key], value);
+const assignProperties = (target: any, source: any) => {
+    for (const [key, value] of Object.entries(source)) {
+        if (typeof(target[key]) === "undefined") {
+            target[key] = value;
+        } else if (typeof(target[key]) === "object" && typeof(value) === "object") {
+            if (Array.isArray(target[key])) {
+                continue;
             }
+            assignProperties(target[key], value);
         }
     }
+}
 
+export default abstract class BaseObject {
     constructor(i: any) {
-        BaseObject.assignProperties(this, i);
+        assignProperties(this, i);
     }
 }
