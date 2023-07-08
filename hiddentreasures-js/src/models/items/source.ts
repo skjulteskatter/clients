680496efb3c675ab0dafee6c7b816261b68c9b3d
Fredrik Vedvik <fredrik@vedvik.tech>
2023-07-08 13:44:28 +0200

    image

--- hiddentreasures-js/src/models/items/source.ts
+++ hiddentreasures-js/src/models/items/source.ts
@@ -2,11 +2,16 @@ import { ISourceService } from "../..";
 import { BaseItem, IBaseItem } from "./baseItem";
 
 export interface ISource extends IBaseItem {
-
+    image: string | null;
 }
 
 export class Source extends BaseItem<ISourceService> implements ISource {
-    
+    public image: string | null;
+
+    constructor(i: ISource, s: ISourceService) {
+        super(i, s);
+        this.image = i.image ?? null;
+    }
 }
 
 export default Source;
